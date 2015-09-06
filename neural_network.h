a8dd8988e5c863532324d1f52623a12670decd3d
Sean Kirmani <sean@kirmani.io>
2015-09-06 20:38:21 +0000

    Added neural net

---
+++ neural_network.h
@@ -0,0 +1,11 @@
+/*
+ * neural_network.h
+ * Copyright (C) 2015 Sean Kirmani <sean@kirmani.io>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+#ifndef NEURAL_NETWORK_H
+#define NEURAL_NETWORK_H
+
+#endif /* !NEURAL_NETWORK_H */
